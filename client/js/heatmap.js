d44fd0693e286d6c0798302fac6abdb48399cbc4
stevenhao <stevenhao@gmail.com>
2016-11-05 15:29:29 -0400

    WIP
    WIP
    
    WIP

--- client/js/heatmap.js
+++ client/js/heatmap.js
@@ -1,25 +1,77 @@
 var MAPBOX = 'https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoicy13dSIsImEiOiJjaXY1Znh4b3AwMWlxMnV0amVtaWFydW80In0.Ew_tvrfwY0yD6R7pa4LLSw'
 var OSM = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+var eps = 0.001
+
+function drawHeatMap(locations) {
+  function onMount(el) {
+    var map = L.map(el).setView([51.505, -0.09], 18);
+    L.tileLayer(MAPBOX, {
+      maxZoom: 18
+    }).addTo(map);
+
+    var N = -200, S = 200;
+    var E = -200, W = 200;
+
+    function doBounds() {
+      var bounds = L.latLngBounds(L.latLng(S-eps,W-eps), L.latLng(N+eps,E+eps));
+      console.log('doing bounds', bounds);
+      map.fitBounds(bounds);
+    }
+
+    console.log('setLocations', locations);
+    var heatLayer = L.heatLayer(locations.map(function(loc) {
+      console.log('loc', loc);
+      N = Math.max(N, loc['latitude'])
+      S = Math.min(S, loc['latitude'])
+      E = Math.max(E, loc['longitude'])
+      W = Math.min(W, loc['longitude'])
+      L.marker(L.latLng(loc['latitude'],loc['longitude'])).addTo(map);
+      return [loc['latitude'],loc['longitude'], loc['score']];
+    }));
+    heatLayer.addTo(map);
+
+    doBounds();
+  }
+
+  var id = locations.join('');
+  return m('.heatmap', {
+    config: onMount,
+    key: id,
+  });
+}
+
 
 var HeatMap = {
   controller: function() {
-    var map;
-    this.setMap = function(_map) { map = _map; };
-    this.getMap = function() { return map };
+    this.addr = m.prop('San Jose');
+    this.query = m.prop('Sushi');
+    this.getLocations = function() {
+      return [
+        {'latitude': 50.45, 'longitude': 30.67, 'score': 500},
+        {'latitude': 50.46, 'longitude': 30.66, 'score': 300},
+      ];
+    };
+    this.updateLocations = function() {
+      m.beginComputation();
+      console.log('todo hit server');
+      m.endComputation();
+    };
   },
   view: function(ctrl) {
-    var map = ctrl.getMap();
-    return m('.heatmap', {
-        config: function(el) {
-          var map = L.map(el).setView([51.505, -0.09], 13);
-          L.tileLayer(MAPBOX , {
-          }).addTo(map);
-
-          ctrl.setMap(map);
-        }
-      }, [
-      m('div', 'This is the heat map.'),
-      m('.map#map', m('div', 'This is the heat map container')),
+    var locations = ctrl.getLocations();
+    return m('div', [
+      m('input', {
+        value: ctrl.addr(),
+        oninput: m.withAttr('value', ctrl.addr),
+      }),
+      m('input', {
+        value: ctrl.query(),
+        oninput: m.withAttr('value', ctrl.query),
+      }),
+      m('button', {
+        onclick: m.updateLocations,
+      }, 'Refresh the shit'),
+      drawHeatMap(locations),
     ]);
   }
 };
