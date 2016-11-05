f1b574b0a28e7acd4aad77b0821e9160631e9819
stevenhao <stevenhao@gmail.com>
2016-11-05 16:28:00 -0400

    WIP

--- client/js/client.js
+++ client/js/client.js
@@ -6,18 +6,9 @@ var client = new rpc.Client({
   host: 'localhost',
   path: '/server',
   strict: false});
-params1 = ['test'];
-console.log('params: ', params1);
-client.call({'method': 'cnt', 'params': params1},
-  function(err, res) {
-    console.log(err, res);
-    var error = res.error;
-    var result = res.result;
-    if (error) {
-      console.error('error:', error);
-    } else {
-      console.log('result:', 'scott', res.result);
-    }
-  }
-);
 
+client.run = function(method, params, cbk) {
+  client.call({'method': method, 'params': params}, cbk);
+}
+
+module.exports = client;
--- client/js/heatmap.js
+++ client/js/heatmap.js
@@ -1,10 +1,18 @@
+var client = require('./client')
+
 var MAPBOX = 'https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoicy13dSIsImEiOiJjaXY1Znh4b3AwMWlxMnV0amVtaWFydW80In0.Ew_tvrfwY0yD6R7pa4LLSw'
 var OSM = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
 var eps = 0.001
 
+var map;
 function drawHeatMap(locations) {
   function onMount(el) {
-    var map = L.map(el).setView([51.505, -0.09], 18);
+    if (map) {
+      map.off();
+      map.remove();
+    }
+    console.log('mounted');
+    map = new L.map(el).setView([51.505, -0.09], 18);
     L.tileLayer(MAPBOX, {
       maxZoom: 18
     }).addTo(map);
@@ -43,19 +51,35 @@ function drawHeatMap(locations) {
 
 var HeatMap = {
   controller: function() {
-    this.addr = m.prop('San Jose');
-    this.query = m.prop('Sushi');
-    this.getLocations = function() {
-      return [
+    var addr = m.prop('410 Memorial Drive, Cambridge, MA 02139');
+    var query = m.prop('Ethiopian');
+    var locations = m.prop([
         {'latitude': 50.45, 'longitude': 30.67, 'score': 500},
         {'latitude': 50.46, 'longitude': 30.66, 'score': 300},
-      ];
+      ]);
+    this.getLocations = function() {
+      return locations();
     };
+
     this.updateLocations = function() {
-      m.beginComputation();
       console.log('todo hit server');
-      m.endComputation();
+      m.startComputation();
+
+      client.run('set_address', [addr(), query().split()], function(err, res) {
+        console.log('set address ok', res.result);
+        client.run('get_businesses', [], function(err, res) {
+          var error = res.error;
+          var result = res.result;
+
+          console.log('got result', result);
+          locations(result);
+          m.endComputation();
+        })
+      })
     };
+
+    this.addr = addr;
+    this.query = query;
   },
   view: function(ctrl) {
     var locations = ctrl.getLocations();
@@ -69,7 +93,7 @@ var HeatMap = {
         oninput: m.withAttr('value', ctrl.query),
       }),
       m('button', {
-        onclick: m.updateLocations,
+        onclick: ctrl.updateLocations,
       }, 'Refresh the shit'),
       drawHeatMap(locations),
     ]);
